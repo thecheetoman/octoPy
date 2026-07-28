83f2810464ce7b439d47ef7bace493f1a3207349
sirbobisherenow <chetan.likes.nature@gmail.com>
2026-07-28 14:22:48 -0700

    remove compiling stuff

--- buildForWindows.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Clearing old build
-rmdir /s /q "./dist/"
-echo Building OctoPy for windows
-pyinstaller --onefile --log-level ERROR --noconsole --name="OctoPy" main.py
-echo Build complete
-echo Transfering ROMS
-xcopy ".\roms" ".\dist\roms" /I
-xcopy ".\testroms" ".\dist\testroms" /I
